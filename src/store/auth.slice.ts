0267ba7a0e3251b9ec1afc228ee3ecef22e05541
peter <peterwangyixing@gmail.com>
2023-11-03 22:06:45 +0800

    Update auth.slice.ts(unfinished)

--- src/store/auth.slice.ts
+++ src/store/auth.slice.ts
@@ -1,12 +1,40 @@
+import {PayloadAction} from "@reduxjs/toolkit";
+
 type User = {
   userId: string;
   username: string;
 }
-interface AuthSlice {
+interface AuthState {
   value: {
     user: User;
     token: string;
   };
 }
+
+const safeJSONParse = (item: string | null) => {
+  if (item === null) {
+    return null;
+  }
+  try {
+    return JSON.parse(item);
+  } catch (error) {
+    console.error('JSON parsing error:', error);
+    return null;
+  }
+}
+
+const createInitState = () => {
+  return {
+    value: safeJSONParse(localStorage.getItem("auth")),
+  };
+};
+
+const createReducers = {
+  setAuth: (state: AuthState, action: PayloadAction<{ user: User; token: string; }>) => {
+    state.value = action.payload;
+  },
+}
+
+
 
 export {};
